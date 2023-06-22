aa180802200e79b6b70e9398b5207a21ee85ea12
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-06-21 21:15:47 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using moviesAPI___Entities.Models;
+
+namespace moviesAPI___Entities.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class MovieController : ControllerBase
+    {
+
+        private static List<Movie> _movies = new List<Movie>();
+
+        public MovieController()
+        {
+            
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie([FromBody] Movie movie)
+        {
+            _movies.Add(movie);
+
+            return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
+        }
+
+        [HttpGet]
+        public IActionResult GetMovies([FromQuery] int skip = 0, [FromQuery] int take = 25)
+        {
+            return Ok(_movies.Skip(skip).Take(take));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovieById(int id)
+        {
+            var movie = _movies.FirstOrDefault(movie => movie.Id == id);
+
+            if(movie == null) return NotFound();
+
+            return Ok(movie);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace moviesAPI___Entities.Models
+{
+    public class Movie
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Genre is required")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Duration is required")]
+        [Range(70, 600)]
+        public int Duration { get; set; }
+    }
+}
